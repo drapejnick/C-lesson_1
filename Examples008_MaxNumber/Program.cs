60a3c3b3f496088e5d21d51579a180f8c4a3656e
Vladimir <drapejnick@gmail.com>
2022-09-04 16:19:22 +0300

    add Example 008

---
+++ Examples008_MaxNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿// Нахождение максимального из 9 чисел. 
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 11, 21, 31, 14, 52, 56, 72, 18, 59 };
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine(max);
