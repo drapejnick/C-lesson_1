1a9d897791bf145bcafab1f05d0c82142300b54f
Vladimir <drapejnick@gmail.com>
2022-09-04 16:56:34 +0300

    add Example 009

--- Example009_Array/Program.cs
+++ Example009_Array/Program.cs
@@ -1,15 +1,47 @@
-﻿int[] array = { 0, 11, 22, 31, 14, 45, 61, 17, 38 };
-
-int n = array.Length;
-int find = 3;
-
-int index = 0;
-
-while (index < n)
+﻿void FillArray(int[] collection)
 {
-    if (array[index] == find)
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
     {
-        Console.WriteLine(index);
+        collection[index] = new Random().Next(1, 10);
+        index++;
     }
-    index++;
 }
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 3);
+Console.WriteLine(pos);
