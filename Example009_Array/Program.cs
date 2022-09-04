8a705bd0b6aa926c1c1e2de0e34ec87a56073006
Vladimir <drapejnick@gmail.com>
2022-09-04 16:31:13 +0300

    add Example 009

---
+++ Example009_Array/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 0, 11, 22, 31, 14, 45, 61, 17, 38 };
+
+int n = array.Length;
+int find = 3;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
